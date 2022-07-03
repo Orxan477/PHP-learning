0edf408c3146416a1c3e316d88e790d5227cc66d
Orkhan Ganbarov <tu201906147@code.edu.az>
2022-07-04 01:53:22 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+#region if else
+// $point=50;
+// if ($point>=81) {
+//     echo "Əla";
+// }
+// else if($point>=71 && $point<81){
+//     echo "Yaxşı";
+// }
+// else if($point>=51 && $point<61){
+//     echo "Orta";
+// }
+// else{
+//     echo "Qeyri-kafi";
+// }
+#endregion
+?>
