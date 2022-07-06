ffe3dbe5e421ec7c6220e20c7840456c0246be0b
Orxan477 <tu201906147@code.edu.az>
2022-07-06 15:19:16 +0400

    Day 2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-#region if else
+#region if else (day 1)
 // $point=50;
 // if ($point>=81) {
 //     echo "Əla";
@@ -13,5 +13,58 @@
 // else{
 //     echo "Qeyri-kafi";
 // }
+#endregion
+
+#region day 2
+    #region task 1
+    // $num_1=0;
+    // $num_2=30;
+    // if ($num_1==30 || $num_2==30 || $num_1 + $num_2==30) {
+    //     echo "true";
+    // }
+    // else{
+    //     echo "false";
+    // }
+    #endregion
+
+    #region task 2
+        // $sum=0;
+        // for ($i=1; $i <=30 ; $i++) { 
+        //     echo $i," ";    
+        //     $sum+=$i;
+        // }
+        // echo "Cavab:",$sum;
+    #endregion
+
+    #region task 3
+            // $age=25;
+            // switch ($age) {
+            //     case '18':
+            //         echo "Age 18";
+            //         break;
+            //     case '20':
+            //         echo "Age 20";
+            //         break;
+            //     case '21':
+            //         echo "Age 21";
+            //         break;
+            //     default:
+            //     echo "Yaş həddini keçdiniz!!";
+            //         break;
+            // }
+    #endregion
+
+    #region task 4
+        // $num_1=20;
+        // $num_2=10;
+        // $sum=$num_1-$num_2;
+        // echo $sum;
+    #endregion
+
+    #region task 5
+        for ($i=1; $i <=10; $i++) { 
+            echo $i,"   ";
+        }
+    #endregion
 #endregion
 ?>
